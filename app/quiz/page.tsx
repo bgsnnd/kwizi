e5889833b767084e1f325ae2835af3f023676964
bgsnnd <bgsnnd8@gmail.com>
2025-03-06 13:38:41 +0700

    y

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -2,6 +2,7 @@
 import { useGlobalContext } from '@/context/globalContext';
 import { useRouter } from 'next/navigation';
 import React from 'react'
+import { useEffect } from 'react';
 
 const page = () => {
     const {selectedQuiz, quizSetup, setQuizSetup, setQuizResponses} = useGlobalContext();
@@ -12,6 +13,35 @@ const page = () => {
     const {shuffleOptions, setShuffleOptions} = React.useState([]);
     const {shuffleQuestions, setShuffleQuestions} = React.useState([]);
 
+    if(!selectedQuiz) {
+      router.push("/");
+      return null;
+    }
+    //shuffle questions when the quiz is started
+    useEffect(()=> {
+        const filteredQuestions = selectedQuiz.questions.filter((q:{difficulty:string})=>{
+          return (!quizSetup?.difficulty || quizSetup?.difficulty === "usspecified" || quizSetup?.difficulty === q.difficulty
+
+          );
+        }
+      )
+      .slice(0, quizSetup?.questionCount);
+
+      setShuffleQuestions(suffleArray([filteredQuestions]));
+    }, [selectedQuiz, quizSetup]);
+
+
+    //Fisher-Yates Shuffle Algorithm
+    const suffleArray = (array: any[]) => {
+      for(let i = array.length -1; i>0;--i){
+        //generate a random index between 0 and i
+        const j = Math.floor(Math.random()* (i+1));
+        //swap element array[i] and array[j]
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    };
+    console.log("Selected Quiz:", selectedQuiz);
 
   return (
     <div>
