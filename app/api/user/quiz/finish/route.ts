b1dffa5cc6446092564f16941601711ea541f6fc
bgsnnd <bgsnnd8@gmail.com>
2025-03-10 12:45:53 +0700

    y

--- app/api/user/quiz/finish/route.ts
+++ app/api/user/quiz/finish/route.ts
@@ -1,5 +1,6 @@
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
 export async function POST(req: NextRequest) {
   try {
--- utils/connect.ts
+++ utils/connect.ts
@@ -1,18 +1,11 @@
-/* eslint-disable no-var */
-const { PrismaClient } = require("@prisma/client");
+import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-declare global{
-    var prisma: PrismaClient;
-}
-if (process.env.NODE_ENV === "production"){
-    prisma = new PrismaClient();
-}else{
-    if(!global.prisma){
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
 
 export default prisma;
